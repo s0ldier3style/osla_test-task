e7cfe9865e1a288dca378843b5253c719e5e761c
Vadim Malinovskii <overrated322@mail.ru>
2026-02-24 19:21:57 +0300

    Updates

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import svgr from 'vite-plugin-svgr';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@storybook/addon-docs',
+  ],
+  framework: '@storybook/react-vite',
+  viteFinal: (config) => {
+    config.plugins = [...(config.plugins || []), svgr()];
+    return config;
+  },
+};
+export default config;
